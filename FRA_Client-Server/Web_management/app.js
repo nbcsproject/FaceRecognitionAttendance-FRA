3bc0961e7c33d5320f642331a244666198b328fc
PrisonerAZ <290422862@qq.com>
2019-04-19 16:55:14 +0800

    add back-end code Basic function is normal
    Signed-off-by: PrisonerAZ <290422862@qq.com>

--- FRA_Client-Server/Web_management/app.js
+++ FRA_Client-Server/Web_management/app.js
@@ -1,5 +1,12 @@
 var express = require('express')
 var path = require('path')
+var bodyParser = require('body-parser')
+var session = require('express-session')
+
+//  加载路由
+var account = require('./routes/account')
+var main = require('./routes/main')
+var user = require('./routes/user')
 
 var app = express()
 
@@ -9,10 +16,21 @@ app.use('/node_modules/', express.static(path.join(__dirname, './node_modules/')
 app.engine('html', require('express-art-template'))
 app.set('views', path.join(__dirname, './views/'))
 
-app.get('/', function (req, res) {
-	res.render('index.html')
-})
+// 配置解析表单 post请求体插件（一定要在app.use(session)）
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+}))
+
+// 把路由挂到app 中
+app.use(account)
+app.use(main)
+app.use(user)
 
 app.listen(3000, function () {
-	console.log('Running...');
+    console.log('Running...');
 })
